e1eb7b9b7f9b1c4153a19c99a894d7163da4e4d1
sowji <jujjurisowjanya@gmail.com>
2023-12-13 20:35:25 +0530

    5th prog

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -4,5 +4,6 @@ echo "first value:${FRUITS[0]}"
 echo "2nd value:${FRUITS[1]}"
 echo "3rd value:${FRUITS[2]}"
 echo "all fruits:${FRUITS[@]}"
-echo "length of the array is::$#{FRUITS}"
-echo "length of the array is::count($FRUITS)"
+echo "length of the array is::${#FRUITS[@]}"
+
+
