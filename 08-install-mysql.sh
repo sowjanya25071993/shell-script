df408583ad07055660aab6ce46c0e9c035bc011f
sowji <jujjurisowjanya@gmail.com>
2023-12-13 20:53:55 +0530

    8th prog

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -3,7 +3,14 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
 echo "error::pls run this script with root access"
+exit 1
 else
 echo "u r root user"
 fi
 yum install mysql -y
+if [ $? -eq 0 ]
+then 
+echo "installing mysql is success"
+else
+echo " error:: installing mysql is failed"
+fi
