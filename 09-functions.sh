3dd317067e9913f7372c9ca648f388ff84e03a86
sowji <jujjurisowjanya@gmail.com>
2023-12-13 21:11:12 +0530

    8th prog

---
+++ 09-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ID=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo "error::$2....failed"
+    else
+    echo "$2............success"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+echo "error::pls run this script using root access"
+else
+echo "u r root user"
+fi
+yum install mysql -y
+VALIDATE $? "installing mysql"
+yum install git -y
+VALIDATE $? "installing git"
