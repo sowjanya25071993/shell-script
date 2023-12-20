8990f670df2d1a243b09330cdc45990cb15820a4
sowji <jujjurisowjanya@gmail.com>
2023-12-20 22:59:04 +0530

    new file

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -2,7 +2,7 @@
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=''
-while IFS = read line
+while IFS= read line
 do
 usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
 partition=$(echo $line | awk '{print $1F}')
