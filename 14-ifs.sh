05e24f678aa4a21f53c91bc4f49096a92662bc5b
sowji <jujjurisowjanya@gmail.com>
2023-12-20 16:00:43 +0530

    new file

--- 14-ifs.sh
+++ 14-ifs.sh
@@ -8,7 +8,7 @@ if [ ! -f $FILE ]
 then 
 echo -e "$R error:: $FILE doesn't exists....$N"
 fi
-while IFS = ":" read -r username password userID groupID useridinfo homedirectory shell
+while IFS=":" read -r username password userID groupID useridinfo homedirectory shell
 do 
 echo "username is::$username"
 echo "userid is::$userID"
