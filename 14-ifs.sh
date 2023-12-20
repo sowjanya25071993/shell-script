9b34e39b812d48e719ecdd16f1a1d4a32cd099f2
sowji <jujjurisowjanya@gmail.com>
2023-12-20 15:57:14 +0530

    new file

---
+++ 14-ifs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+$FILE=/etc/passwd
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+if [ ! -f $FILE ]
+then 
+echo -e "$R error:: $FILE doesn't exists....$N"
+fi
+while IFS = ":" read -r username password userID groupID useridinfo homedirectory shell
+do 
+echo "username is::$username"
+echo "userid is::$userID"
+echo "user full name is::$useridinfo"
+done < $FILE
