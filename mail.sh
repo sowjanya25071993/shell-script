ba0c70dcf0ef3bb73569de66cb5b3b64222e2e1f
sowji <jujjurisowjanya@gmail.com>
2023-12-20 23:36:04 +0530

    new file

--- mail.sh
+++ mail.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-to_team=$1
-alert_type=$2
-body=$3
+TO_TEAM=$1
+ALERT_TYPE=$2
+BODY=$3
 escape_body=$(printf '%s\n' "$body" | sed -e 's/[]\/$*.^[]/\\&/g');
 to_address=$4
 subject=$5
-FINAL_BODY=$(sed -e "s/to_team/$to_team/g" -e "s/alert-type/$alert_type/g" -e "s/body/$escape_body/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$aALERT_TYPE/g" -e "s/BODY/$escape_body/g" template.html)
 echo "$FINAL_BODY" | mail -s "$(echo -e "$subject \n content_type: text/html")" "$to_address"
