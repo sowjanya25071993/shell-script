526868d4bbab74734514056a678ea83db6518e03
sowji <jujjurisowjanya@gmail.com>
2023-12-20 23:13:02 +0530

    new file

--- mail.sh
+++ mail.sh
@@ -2,7 +2,7 @@
 to_team=$1
 alert_type=$2
 body=$3
-escape_body=$(printf '%s\n' "$body" | sed -e 's/[]\/ $*.^[]/\\&\g');
+escape_body=$(printf '%s\n' "$body" | sed -e 's/[]\/$*.^[]/\\&\g');
 to_address=$4
 subject=$5
 FINAL_BODY=$(sed -e "s/to_team/$to_team/g" -e "s/alert-type/$alert_type/g" -e "s/body/$escape_body/g" template.html)
