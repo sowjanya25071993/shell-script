9b94120e9d2354504c5892777bea7d250f7ba7bf
sowji <jujjurisowjanya@gmail.com>
2023-12-20 22:59:34 +0530

    new file

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -6,7 +6,7 @@ while IFS= read line
 do
 usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
 partition=$(echo $line | awk '{print $1F}')
-if [ $usage -ge $DISK_THRESHOLD]
+if [ $usage -ge $DISK_THRESHOLD ]
 then
 message+="high disk usage on $partition:$usage <br>"
 fi
